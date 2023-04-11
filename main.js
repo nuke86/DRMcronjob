cce27ae053d0a2f52077dd06d4b47fcc4743fff2
Dario Fadda <nuke@spcnet.it>
2023-04-11 14:50:39 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,46 @@
+/*-------------------------------------------------------------------------------------------------------------\
+|  _______    _    _____ _             _ _           ________     ___   ___ ___  __     _____   ___ ___  ___   |
+| |__   __|  (_)  / ____| |           | (_)         /  ____  \   |__ \ / _ \__ \/_ |   / /__ \ / _ \__ \|__ \  |
+|    | | __ _ _  | (___ | |_ _   _  __| |_  ___    /  / ___|  \     ) | | | | ) || |  / /   ) | | | | ) |  ) | |
+|    | |/ _` | |  \___ \| __| | | |/ _` | |/ _ \  |  | |       |   / /| | | |/ / | | / /   / /| | | |/ /  / /  |
+|    | | (_| | |  ____) | |_| |_| | (_| | | (_) | |  | |___    |  / /_| |_| / /_ | |/ /   / /_| |_| / /_ / /_  |
+|    |_|\__,_|_| |_____/ \__|\__,_|\__,_|_|\___/   \  \____|  /  |____|\___/____||_/_/   |____|\___/____|____| |
+|                                                   \________/                                                 |
+\-------------------------------------------------------------------------------------------------------------*/
+
+const puppeteer = require('puppeteer');
+require('dotenv').config();
+
+(async () => {
+    if(process.env.WORKFLOW_INPUT != null){
+        var tmp = JSON.parse(process.env.WORKFLOW_INPUT);
+        process.env.websiteURL = tmp.websiteURL;
+        process.env.width = tmp.width;
+        process.env.height = tmp.height;
+
+        console.log('Run With Github Action !');
+    }
+
+    if(process.env.websiteURL == null){
+        process.env.websiteURL = 'https://tai-studio.netlify.app/';
+    }
+    if(process.env.width != null){
+        process.env.width = '1920';
+    }
+    if(process.env.height != null){
+        process.env.height = '1080';
+    }
+
+    const browser = await puppeteer.launch({
+        defaultViewport: {
+          width: 1920,
+          height: 1080
+        }
+    });
+    const page = await browser.newPage();
+    await page.goto(process.env.websiteURL);
+    await page.setViewport({width: 1920, height: 1080});
+    await page.screenshot({ path: 'screen.png', fullPage: true });
+
+    await browser.close();
+})();
